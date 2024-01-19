a7ddfce38204a9eea5893bf69fb5f3653686630e
zzinue <zzinue@gmail.com>
2024-01-19 12:12:10 -0600

    limpiar input y habilitar boton de nuevo juego

--- app.js
+++ app.js
@@ -9,7 +9,7 @@
 
 let numeroSecreto = numeroRandom();
 console.log(numeroSecreto);
-let intentos=1; 
+let intentos = 1;
 
 function aparecerTexto(header, texto) {
   let general = document.querySelector(header);
@@ -20,23 +20,37 @@ function verificarIntento() {
   let numeroDeUsuario = parseInt(
     document.getElementById("valorUsuarioId").value
   );
- if(numeroDeUsuario===numeroSecreto){
-aparecerTexto("p", `Acertaste el numero en ${intentos} ${(intentos ===1) ?'vez':'veces'}`);
- }else{
-  if(numeroDeUsuario>numeroSecreto){
-    aparecerTexto("p", "el numero es menor");
-  }else{
-  aparecerTexto("p", "el numero es mayor");
- }
- intentos++
-}
+  if (numeroDeUsuario === numeroSecreto) {
+    aparecerTexto(
+      "p",
+      `Acertaste el numero en ${intentos} ${intentos === 1 ? "vez" : "veces"}`
+    );
+    // habilitar boton de nuevo juego
+    habilitarBoton();
+  } else {
+    if (numeroDeUsuario > numeroSecreto) {
+      aparecerTexto("p", "el numero es menor");
+    } else {
+      aparecerTexto("p", "el numero es mayor");
+    }
+    intentos++;
+    // limpiar la caja
+    removerNumero();
+  }
   return;
 }
+
+function habilitarBoton() {
+  document.getElementById("reiniciar").removeAttribute("disabled");
+}
+
+function removerNumero() {
+  document.querySelector("#valorUsuarioId").value = "";
+}
+
 function numeroRandom() {
   let numeroAleatorioDigito = Math.floor(Math.random() * 10) + 1;
   return numeroAleatorioDigito;
 }
 aparecerTexto("h1", "Juego del numero secreto");
 aparecerTexto("p", "Indica un numero del 1 al 10");
- 
-
